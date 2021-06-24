a5285c161c0c51b1049d12372eec71965b887a57
Miguel Campos <63472933+MCampos23@users.noreply.github.com>
2021-06-24 19:17:40 +0200

    Update customers.js

--- routes/customers.js
+++ routes/customers.js
@@ -6,19 +6,17 @@ const {Sale} = require('../models/sale')
 //Filter by satisfaction rate
 router.get('/satisfaction/:num', async (req, res) => {
     const saleSatisfaction = await Sale.find({"customer.satisfaction": req.params.num}).select('customer')                                    
-   
     res.send(saleSatisfaction)
   })
 
-  //Filter by customer gender
+//Filter by customer gender
 router.get('/:gender', async (req, res) => {
     const customerGender = await Sale.find({"customer.gender": req.params.gender}).select('customer')                                             
-    
     res.send(customerGender)
   })
   
-  //Filter by customer age < 35
-  router.get('/age/young', async (req, res) => {
+//Filter by customer age < 35
+router.get('/age/young', async (req, res) => {
     const youngCustomers = await Sale.find({"customer.age": {$lt: 35}}).select('customer')                                             
     
     res.send(youngCustomers)
